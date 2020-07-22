008c66cb30300c76ad99c914a3452f55cecb5b9d
syahren-aa <asyahren@gmail.com>
2020-07-22 14:16:14 +0700

    ban

--- TR_GRAFKOM/TR_GRAFKOM/kwlBody_depan.cpp
+++ TR_GRAFKOM/TR_GRAFKOM/kwlBody_depan.cpp
@@ -28,6 +28,21 @@
 
 */
 
+void ban(int jari2, int lebar_ban, int x_tengah, int y_tengah, int z_tengah) {
+	float PI = 3.14;
+	glBegin(GL_POLYGON);
+	float z = z_tengah;
+		for (int i = 0; i <= 3600; i++) {
+			float sudut = i * (2 * PI / 3600);
+			float x = x_tengah + jari2 * cos(sudut);
+			float y = y_tengah + jari2 * sin(sudut);
+			glVertex3f(x, y, z);
+			glVertex3f(x, y, z + lebar_ban);
+		}
+	glEnd();
+}
+
+
 void selebor(float koor_x,float koor_y,float koor_z,float panjang,float lebar,float tinggi)
 {
 	Warna(0, 0, 212);
@@ -38,12 +53,12 @@ void selebor(float koor_x,float koor_y,float koor_z,float panjang,float lebar,fl
 	glVertex3d((koor_x + ((panjang / 3) / 2)), koor_y + (tinggi / 4), (koor_z + 2 * lebar));
 	glEnd();
 	box_topBottom(panjang / 2, lebar, koor_x + (panjang / 3), koor_y + (tinggi / 2), (koor_z + 2 * lebar));
-	glBegin(GL_POLYGON);
-	glVertex3d((koor_x + (panjang / 3) + (panjang / 2)), koor_y + (tinggi / 2), (koor_z + 2 * lebar));
-	glVertex3d((koor_x + (panjang / 3) + (panjang / 2)), koor_y + (tinggi / 2), (koor_z + lebar));
-	glVertex3d((koor_x + (panjang / 3) + (panjang / 2) + (panjang / 3) / 2), koor_y + (tinggi / 4), (koor_z + lebar));
-	glVertex3d((koor_x + (panjang / 3) + (panjang / 2) + (panjang / 3) / 2), koor_y + (tinggi / 4), (koor_z + 2 * lebar));
-	glEnd();
+
+	ban(30, 3, koor_x + (panjang / 1.8), koor_y, (koor_z + 1.25 * lebar)-2);
+	ban(30, 3, koor_x + (panjang / 1.8), koor_y, (koor_z + lebar*1.25 + 30));
+	Warna();
+	ban(40, 30, koor_x + (panjang / 1.8), koor_y , (koor_z +1.25* lebar));
+	
 }
 
 void init_kwlBody_depan() {
@@ -89,5 +104,16 @@ void init_kwlBody_depan() {
 
 	//tutup
 	box_topBottom(panjang/3,lebar,koor_x, koor_y+tinggi, koor_z+lebar);
+	Warna(255, 255, 255);
+	box_frontBack(7,15, (koor_x + (panjang / 5)), (koor_y + tinggi), (koor_z + lebar/5));
+	box_frontBack(7,15,  (koor_x + (panjang / 5)), (koor_y + tinggi), (koor_z + lebar/10));
+	box_sides(5,15, (koor_x + (panjang / 5)), (koor_y + tinggi), (koor_z + lebar / 5));
+	box_sides(5, 15, (koor_x + (panjang / 3.7)), (koor_y + tinggi), (koor_z + lebar / 5));
 
+	ban(3, lebar, (koor_x + (panjang / 4)), (koor_y + tinggi+15), koor_z);
+
+	box_frontBack(7, 15, (koor_x + (panjang / 5)), (koor_y + tinggi), (koor_z + lebar * 0.8));
+	box_frontBack(7, 15, (koor_x + (panjang / 5)), (koor_y + tinggi), (koor_z + lebar *0.9));
+	box_sides(5, 15, (koor_x + (panjang / 5)), (koor_y + tinggi), (koor_z + lebar *0.9));
+	box_sides(5, 15, (koor_x + (panjang / 3.7)), (koor_y + tinggi), (koor_z + lebar  *0.9));
 	}
