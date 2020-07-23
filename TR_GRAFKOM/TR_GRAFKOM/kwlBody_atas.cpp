28fa4af40c09dc2073f0aecdb6ee8d536764705f
hasansansan <672018337@studen.uksw.edu>
2020-07-23 13:00:45 +0700

    bagian_atas

--- TR_GRAFKOM/TR_GRAFKOM/kwlBody_atas.cpp
+++ TR_GRAFKOM/TR_GRAFKOM/kwlBody_atas.cpp
@@ -40,99 +40,104 @@ void init_kwlBody_atas() {
 	glTranslated(10, 25, 15);
 	glRotatef(180, 0, 1, 0);
 
-	//depan atas miring
-	Warna(30,20);
+
+
+
+	//jendela
+	Warna(235, 198, 52);
+	box_frontBack((panjang - 90), tinggi - 15, koor_x - 80, koor_y + 5, koor_z - 25);
+	box_frontBack((panjang - 95), tinggi - 15, koor_x - 40, koor_y + 5, koor_z - 25);
+
 	glBegin(GL_POLYGON);
-	glVertex3d((koor_x + (panjang / 3)-55), koor_y+5, koor_z-25);
-	glVertex3d((koor_x + (panjang / 3)-55), (koor_y + tinggi-10), koor_z-25);
-	glVertex3d(koor_x + panjang-95, koor_y+5, koor_z-25);
+	glVertex3d(koor_x - 40, koor_y + 80, koor_z - 25);
+	glVertex3d(koor_x - 40, koor_y + 90, koor_z - 25);
+	glVertex3d(koor_x - 80, koor_y + 90, koor_z - 25);
+	glVertex3d(koor_x - 80, koor_y + 80, koor_z - 25);
 	glEnd();
 
 	glBegin(GL_POLYGON);
-	glVertex3d(koor_x + panjang-95, koor_y+5, (koor_z + lebar-5));
-	glVertex3d((koor_x + (panjang / 3) - 55), koor_y+5, (koor_z + lebar-5));
-	glVertex3d((koor_x + (panjang / 3) - 55), (koor_y + tinggi-10), (koor_z + lebar-5));
+	glVertex3d(koor_x +10,koor_y+5,koor_z-25);
+	glVertex3d(koor_x +1, koor_y + 30, koor_z - 25);
+	glVertex3d(koor_x - 40, koor_y + 30, koor_z - 25);
+	glVertex3d(koor_x - 40, koor_y + 5, koor_z - 25);
 	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex3d(koor_x - 13, koor_y + 70, koor_z - 25);
+	glVertex3d(koor_x - 20, koor_y +90, koor_z - 25);
+	glVertex3d(koor_x - 40, koor_y + 90, koor_z - 25);
+	glVertex3d(koor_x - 40, koor_y + 70, koor_z - 25);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex3d(koor_x+1 , koor_y + 30, koor_z - 25);
+	glVertex3d(koor_x - 20, koor_y + 90, koor_z - 25);
+	glVertex3d(koor_x - 30, koor_y + 90, koor_z - 25);
+	glVertex3d(koor_x -11, koor_y + 30, koor_z - 25);
+	glEnd();
+
+
+
+	//jendela sebelah
+
+	box_frontBack((panjang - 90), tinggi - 15, koor_x - 80, koor_y + 5, koor_z + lebar - 5);
+	box_frontBack((panjang - 95), tinggi - 15, koor_x - 40, koor_y + 5, koor_z + lebar - 5);
+
+	glBegin(GL_POLYGON);
+	glVertex3d(koor_x - 40, koor_y + 80, koor_z +lebar-5);
+	glVertex3d(koor_x - 40, koor_y + 90, koor_z +lebar - 5);
+	glVertex3d(koor_x - 80, koor_y + 90, koor_z +lebar- 5);
+	glVertex3d(koor_x - 80, koor_y + 80, koor_z+ lebar - 5);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex3d(koor_x - 40, koor_y + 40, koor_z + lebar - 5);
+	glVertex3d(koor_x - 40, koor_y + 30, koor_z + lebar - 5);
+	glVertex3d(koor_x - 80, koor_y + 30, koor_z + lebar - 5);
+	glVertex3d(koor_x - 80, koor_y + 40, koor_z + lebar - 5);
+	glEnd();
+
+
+	glBegin(GL_POLYGON);
+	glVertex3d(koor_x + 10, koor_y + 5, koor_z + lebar - 5);
+	glVertex3d(koor_x + 1, koor_y + 30, koor_z + lebar - 5);
+	glVertex3d(koor_x - 80, koor_y + 30, koor_z + lebar - 5);
+	glVertex3d(koor_x - 80, koor_y + 5, koor_z +lebar-5);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex3d(koor_x - 13, koor_y + 70, koor_z + lebar -5);
+	glVertex3d(koor_x - 20, koor_y + 90, koor_z +lebar-5);
+	glVertex3d(koor_x - 40, koor_y + 90, koor_z + lebar -5);
+	glVertex3d(koor_x - 40, koor_y + 70, koor_z +lebar - 5);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex3d(koor_x + 1, koor_y + 30, koor_z +lebar- 5);
+	glVertex3d(koor_x - 20, koor_y + 90, koor_z +lebar-5);
+	glVertex3d(koor_x - 30, koor_y + 90, koor_z +lebar-5);
+	glVertex3d(koor_x - 11, koor_y + 30, koor_z +lebar-5);
+	glEnd();
+
+	//kaca Depan
+	box_miringKK(koor_x + 10, koor_x - 20, koor_y + 5, koor_y + panjang-10, koor_z + lebar-5, koor_z + lebar-14);
+	box_miringKK(koor_x + 10, koor_x - 20, koor_y + 5, koor_y + panjang - 10, koor_z-25 , koor_z -17);
+	box_miringKK(koor_x + 10, koor_x+2 , koor_y + 5, koor_y + 25, koor_z - 20, koor_z + 40);
+	box_miringKK(koor_x - 16, koor_x-20, koor_y + panjang-20, koor_y +panjang-10, koor_z - 20, koor_z + 40);
+
+	Warna(135, 133, 116);
+	box_sides(lebar+20, tinggi-50, koor_x - (panjang/2)-30, koor_y+5, (koor_z + lebar-5));
+	box_sides(lebar + 20, tinggi - 85, koor_x - (panjang / 2) - 30, koor_y + 75, (koor_z + lebar-5));
+	box_sides(lebar -40, tinggi-15 , koor_x - (panjang / 2) - 30, koor_y + 5, (koor_z + lebar-5));
+	box_sides(lebar - 40, tinggi - 15, koor_x - (panjang / 2) - 30, koor_y + 5, koor_z -15);
 	
-	glBegin(GL_POLYGON);
-	glVertex3d((koor_x + (panjang / 3) - 55), (koor_y + tinggi - 10), (koor_z + lebar));
-	glVertex3d((koor_x + (panjang / 3) - 55), (koor_y + tinggi - 10), koor_z-25);
-	glVertex3d(koor_x + panjang - 95, koor_y + 5, koor_z-25);
-	glVertex3d(koor_x + panjang - 95, koor_y + 5, koor_z + lebar);
-	glEnd();
 
-	//kaca
-	Warna(0, 40, 220);
-	glBegin(GL_POLYGON);
-	glVertex3d((koor_x + (panjang / 3) - 50), (koor_y + tinggi - 20), (koor_z + lebar-5));
-	glVertex3d((koor_x + (panjang / 3) - 50), (koor_y + tinggi - 20), koor_z-20);
-	glVertex3d(koor_x + panjang - 95, koor_y + 15, koor_z-20);
-	glVertex3d(koor_x + panjang - 95, koor_y + 15, koor_z + lebar-5);
-	glEnd();
-
-	//badan atas
-	Warna(30, 50);
-	box_sides(lebar+25, tinggi-50, koor_x - (panjang/2)-34, koor_y+5, (koor_z + lebar));
-	box_sides(lebar + 25, tinggi - 85, koor_x - (panjang / 2) - 34, koor_y + 75, (koor_z + lebar));
-	box_sides(lebar -40, tinggi-15 , koor_x - (panjang / 2) - 34, koor_y + 5, (koor_z + lebar));
-	box_sides(lebar - 40, tinggi - 15, koor_x - (panjang / 2) - 34, koor_y + 5, koor_z -15);
-	Warna(40, 20);
-	box_frontBack((panjang-38), tinggi-15, koor_x-84, koor_y+5, koor_z-25);
-	box_frontBack((panjang-38), tinggi-15, koor_x-84, koor_y+5, koor_z + lebar-5);
-
-	box_topBottom(panjang - 35, lebar + 25, koor_x-panjang+15, koor_y + panjang-10, koor_z+lebar);
+	box_topBottom(panjang - 35, lebar + 20, koor_x-panjang+20, koor_y + panjang-10, koor_z+lebar-5);
 	Warna(50);
-		//pintu//
-	//box_frontBack(panjang - 55, tinggi - 25, koor_x - 115, koor_y + 5, koor_z + lebar);
-	//box_frontBack(panjang - 55, tinggi - 25, koor_x - 115, koor_y + 5, koor_z);
-		//jendela//
-	//box_frontBack(panjang - 78, tinggi - 45, koor_x - 65, koor_y + 25, koor_z );
-	//glBegin(GL_POLYGON);
-	//glVertex3d((koor_x + (panjang / 3) - 77), koor_y + 25, koor_z);
-	//glVertex3d((koor_x + (panjang / 3) - 77), (koor_y + tinggi - 20), koor_z);
-	//glVertex3d(koor_x -15 , koor_y + 25, koor_z);
-	//glEnd();
-
-		//jendela//
-//	box_frontBack(panjang - 78, tinggi - 45, koor_x - 65, koor_y + 25, koor_z+lebar);
-///	glBegin(GL_POLYGON);
-	//glVertex3d((koor_x + (panjang / 3) - 77), koor_y + 25, koor_z+lebar);
-//	glVertex3d((koor_x + (panjang / 3) - 77), (koor_y + tinggi - 20), koor_z+lebar);
-//	glVertex3d(koor_x - 15, koor_y + 25, koor_z+lebar);
-//	glEnd();
 
 	glRotatef(-180, 0, 1, 0);
 	glTranslated(-10, 25, -15);
 
-	/*
-	//bokong belakang
-	Warna(22, 22,21);
-	box_frontBack((panjang - 50), tinggi - 30, koor_x - 170, koor_y - 20, koor_z);
-	box_frontBack((panjang - 50), tinggi - 30, koor_x - 170, koor_y - 20, koor_z + lebar);
-	Warna(0, 2);
-	box_topBottom(panjang - 50, lebar, koor_x - (panjang*2)+30, koor_y + panjang - 50, koor_z+lebar );
-	Warna(20);
-	glBegin(GL_TRIANGLES);
-	box_frontBack(20, 20, koor_x - panjang - 90, koor_y + 30, koor_z);
-	box_frontBack(20, 50, koor_x - panjang - 90, koor_y-20 , koor_z);
-
-	glBegin(GL_TRIANGLES);
-	box_frontBack(20, 20, koor_x - panjang - 90, koor_y + 30, koor_z+lebar);
-	box_frontBack(20, 50, koor_x - panjang - 90, koor_y - 20, koor_z+lebar);
-	glEnd();
-
-	Warna(0, 0, 212);
-	glBegin(GL_POLYGON);
-	glVertex3d((koor_x + (panjang / 2)-(panjang*2)-20), koor_y + (tinggi / 2), (koor_z +lebar));
-	glVertex3d((koor_x + (panjang / 2) - (panjang * 2)-20), koor_y + (tinggi / 2), (koor_z ));
-	glVertex3d((koor_x + ((panjang / 2) / 2) - (panjang * 2)-15), koor_y + (tinggi / 4)+5, (koor_z ));
-	glVertex3d((koor_x + ((panjang / 2) / 2) - (panjang * 2)-15), koor_y + (tinggi / 4)+5, (koor_z + lebar ));
-	glEnd();
-	
-	box_sides(lebar, tinggi - 50, koor_x - panjang - 90, koor_y - 20, (koor_z + lebar));
-
-
-	*/
 
 
 
